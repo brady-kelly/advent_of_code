7a3a080505cac236f448fb14a40082c6b519655f
Brady Kelly <bradyk@staycontactable.com>
2024-12-18 10:53:53 +0200

    Add project files.

---
+++ Day1/Day1.cs
@@ -0,0 +1,37 @@
+﻿namespace Day1
+{
+    public class Day1
+    {
+        private List<int> left = [];
+        private List<int> right = [];
+
+        public void Init(string filePath)
+        {
+            left.Clear();
+            right.Clear();
+
+            var lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                left.Add(int.Parse(nums[0]));
+                right.Add(int.Parse(nums[1]));
+            }
+        }
+
+        public string Solve()
+        {
+            left.Sort();
+            right.Sort();
+
+            int tote = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                tote += Math.Abs(left[i] - right[i]);
+            }
+
+            return tote.ToString();
+        }
+    }
+}
---
+++ First/Program.cs
@@ -0,0 +1,7 @@
+﻿var day = new Day1.Day1();
+
+day.Init(@"C:\Development\AdventOfCode\Inputs\day-1.txt");
+var sol = day.Solve();
+
+Console.WriteLine(sol);
+Console.ReadLine();
