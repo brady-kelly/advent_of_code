ad10c928f40ff437cdc12c3e498f9ce953d6554e
Brady Kelly <bradyk@staycontactable.com>
2025-04-16 03:04:27 +0200

    Day2 Part2 working

--- Day1/Day2.cs
+++ Day1/Day2.cs
@@ -21,29 +21,56 @@
             var safeCount = 0;
             foreach (var rep in _reports)
             {
-                //Console.WriteLine(string.Join(' ', rep.Select(r => r.ToString())));
-
-                var safe = true;
-                var ld = rep[0] - rep[1];
-                if (Math.Abs(ld) < 1 || Math.Abs(ld) > 3)
+                if (IsReportSafe(rep))
                 {
-                    continue;
-                }
-
-                for (var i = 1; i < rep.Count - 1; i++)
-                {
-                    var cd = rep[i] - rep[i + 1];
-                    if (int.Sign(cd) != int.Sign(ld) || Math.Abs(cd) < 1 || Math.Abs(cd) > 3)
-                    {
-                        safe = false;
-                        break;
-                    }
-                    ld = cd;
-                }
-                if (safe)
                     safeCount++;
+                }
             }
             return safeCount.ToString();
+        }
+
+        public string Part2()
+        {
+            var safeCount = 0;
+            foreach (var rep in _reports)
+            {
+                if (IsReportSafe(rep))
+                {
+                    safeCount++;
+                }
+                else
+                {
+                    for(var i = 0; i < rep.Count; i++)
+                    {
+                        var repStart = rep[0..i];
+                        var repEnd = rep[(i + 1)..^0];
+                        var newRep = repStart.Concat(repEnd).ToList();
+                        if (IsReportSafe(newRep))
+                        {
+                            safeCount++;
+                            break;
+                        }
+                    }
+                }
+            }
+            return safeCount.ToString();
+        }
+
+        private bool IsReportSafe(List<int> rep)
+        {
+            var safe = true;
+            var ld = rep[0] - rep[1];
+            for (var i = 1; i < rep.Count - 1; i++)
+            {
+                var cd = rep[i] - rep[i + 1];
+                if (int.Sign(cd) != int.Sign(ld) || Math.Abs(ld) < 1 || Math.Abs(ld) > 3 || Math.Abs(cd) < 1 || Math.Abs(cd) > 3)
+                {
+                    safe = false;
+                    break;
+                }
+                ld = cd;
+            }
+            return safe;
         }
     }
 }
--- First/Program.cs
+++ First/Program.cs
@@ -3,10 +3,10 @@
 var day = new Day2(2);
 day.Init();
 
-var sol1 = day.Part1();
-//var sol2 = day.Part2();
+//var sol1 = day.Part1();
+var sol2 = day.Part2();
 
-Console.WriteLine($"Day 2 Part 1: {sol1}");
-//Console.WriteLine($"Day 1 Part 2: {sol2}");
+//Console.WriteLine($"Day 2 Part 1: {sol1}");
+Console.WriteLine($"Day 1 Part 2: {sol2}");
 
 Console.ReadLine();
