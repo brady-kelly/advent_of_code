94f385fe58ef6e25eb7bf2906b75a55aa7a45414
Brady Kelly <bradyk@staycontactable.com>
2025-04-17 05:55:59 +0200

    Started Day 4

---
+++ Day1/Day4.cs
@@ -0,0 +1,21 @@
+namespace Puzzles;
+
+public class Day4(int i) : Day(i)
+{
+    public override string Part1()
+    {
+        var len = Lines[0].Length;
+        var pos = 0;
+
+        for (var x = -1; x <= 1; x++)
+        {
+            
+        }
+        return "";
+    }
+
+    public override string Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
