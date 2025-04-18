a9e4d9c43038caae9de523807af73bbf7638a6c1
Brady Kelly <bradyk@staycontactable.com>
2025-04-18 17:05:10 +0200

    Day 4 left and right checks working.

--- Day1/Day4.cs
+++ Day1/Day4.cs
@@ -2,14 +2,35 @@ namespace Puzzles;
 
 public class Day4(int i) : Day(i)
 {
+    private const string Term = "XMAS";
+
     public override string Part1()
     {
-        var len = Lines[0].Length;
-        var pos = 0;
-
-        for (var x = -1; x <= 1; x++)
+        var count = 0;
+        
+        var rev = Term.Reverse().ToString();
+        
+        for (var y = 0; y < Lines.Count; y++)
         {
-            
+            for (var x = 0; x < Lines[y].Length; x++)
+            {
+                if (x >= Term.Length)
+                {
+                    var sub = Lines[y].Substring(x - Term.Length, Term.Length);
+                    if (sub == Term || sub == rev)
+                    {
+                        count++;
+                    }
+                    if (Lines[y].Length - x >= Term.Length)
+                    {
+                        var sub2 = Lines[y].Substring(x, Term.Length);
+                        if (sub2 == Term || sub2 == rev)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
         }
         return "";
     }
--- First/Program.cs
+++ First/Program.cs
@@ -3,10 +3,10 @@
 var day = new Day4(4);
 day.Init();
 
-//var sol1 = day.Part1();
-var sol2 = day.Part1();
+var sol1 = day.Part1();
+//var sol2 = day.Part2();
 
 //Console.WriteLine($"Day 3 Part 1: {sol1}");
-Console.WriteLine($"Day 4 Part 1: {sol2}");
+Console.WriteLine($"Day 4 Part 1: {sol1}");
 
 Console.ReadLine();
